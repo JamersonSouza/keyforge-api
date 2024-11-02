e7a35efbca45aa75e4f9ca8a5826b2d158c87442
jamersonsouza <jamersonsouza@outlook.com>
2024-11-02 12:36:05 -0300

    init 🎉:  inicio do projeto keyforge

---
+++ src/main/java/tech/jamersondev/keyforge/KeyforgeApplication.java
@@ -0,0 +1,13 @@
+package tech.jamersondev.keyforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyforgeApplication.class, args);
+	}
+
+}
